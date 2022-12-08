77abe9bc2fa1e4cd66698620b6c6d377efc2b5c0
Andrew <Asaganda@gmail.com>
2022-12-08 15:20:02 -0500

    basic structure of profile page w/ dummy data for now

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,21 @@
-const Profile = () => {
+const Profile = (props) => {
     return (
         <div id="profile">
             <h1>Each Profile</h1>
+            <div className="profile-info">
+                <img src="https://freesvg.org/img/abstract-user-flat-4.png"/>
+                <p>Followers: 10</p>
+                <p>Following: 5</p>
+            </div>
+            <div className="profile-posts-container">
+                <div className="profile-post">
+                    <h3>each post</h3>
+                    <img src="https://www.travelandleisure.com/thmb/SPUPzO88ZXq6P4Sm4mC5Xuinoik=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/eiffel-tower-paris-france-EIFFEL0217-6ccc3553e98946f18c893018d5b42bde.jpg"/>
+                    <p>info: travelling around Paris</p>
+                    <h4>likes:</h4>
+                    <h4>comments:</h4>
+                </div>
+            </div>
         </div>
     )
 }
